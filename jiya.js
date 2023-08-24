11f3c1835dc3d5ca2811bbb9a60f50d310dea00f
Mohamed <mohamed.h@proglint.com>
2023-08-24 16:32:03 +0530

    Text added

--- jiya.js
+++ jiya.js
@@ -6,6 +6,7 @@ export class MyApp extends Component {
     return (
       <View style={{backgroundColor: 'black'}}>
         <Text>{'jiya'}</Text>
+        <Text>{'jiya'}</Text>
       </View>
     );
   }
