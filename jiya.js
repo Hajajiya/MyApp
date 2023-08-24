5c236136f230eb1500cf9cfa847f6843d5f3e6c0
Mohamed <mohamed.h@proglint.com>
2023-08-24 16:31:29 +0530

    first commit

---
+++ jiya.js
@@ -0,0 +1,14 @@
+import react, {Component} from 'react';
+import {View, Text} from 'react-native';
+
+export class MyApp extends Component {
+  render() {
+    return (
+      <View style={{backgroundColor: 'black'}}>
+        <Text>{'jiya'}</Text>
+      </View>
+    );
+  }
+}
+
+export default MyApp();
